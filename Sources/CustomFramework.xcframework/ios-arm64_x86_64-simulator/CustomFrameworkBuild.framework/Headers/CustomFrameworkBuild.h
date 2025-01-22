506f2a26376c71796b018170218221dd83ff61f6
Kamil Góralski <kamil.goralski@norbsoft.com>
2025-01-22 15:33:20 +0100

    Next commit

---
+++ Sources/CustomFramework.xcframework/ios-arm64_x86_64-simulator/CustomFrameworkBuild.framework/Headers/CustomFrameworkBuild.h
@@ -0,0 +1,18 @@
+//
+//  CustomFrameworkBuild.h
+//  CustomFrameworkBuild
+//
+//  Created by Kamil Goralski on 22/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFrameworkBuild.
+FOUNDATION_EXPORT double CustomFrameworkBuildVersionNumber;
+
+//! Project version string for CustomFrameworkBuild.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkBuildVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFrameworkBuild/PublicHeader.h>
+
+
--- Sources/CustomFramework/SDK.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public struct SDK {
-    static public func doSomeWork() {
-        print("Doing some work...")
-    }
-}
